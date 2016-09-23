45f88d75081832edbd660653b9a5131b026af4d7
rafaelbacus <rafael.bacus@gmail.com>
2016-09-23 21:30:20 +0800

    Uncommented testsuites

--- app/src/androidTest/java/com/example/android/sunshine/app/data/TestPractice.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestPractice.java
@@ -19,7 +19,7 @@ public class TestPractice extends AndroidTestCase {
 
         assertEquals("X should be equal", a, c);
         assertTrue("Y should be true", d > a);
-        assertFalse("Z should be false", a == b);
+        assertFalse("Z should be false", a== b);
 
         if (b > d) {
             fail("XX should never happen");
